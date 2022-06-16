7780076441d0ed50ac462c28fa2d74d537004a1a
Chinh Pham <75044457+ch1nhcp@users.noreply.github.com>
2022-06-17 02:05:49 +0700

    Xong Tính năng tự bật đèn khi trời tối
    Xong Tính năng tự bật đèn khi trời tối dựa vào cảm biến ánh sáng

--- Main.ino
+++ Main.ino
@@ -11,8 +11,21 @@ const int DHTTYPE = DHT11;   // Khai báo kiểu cảm biến là DHT11
 
 DHT dht(DHTPIN, DHTTYPE); //Khai báo thư viện chân cảm biến và kiểu cảm biến
 
+// Vỗ tay bật tắt đèn-------
+int soundSensor = 2;
+int LED = 4;
+boolean LEDStatus = false;
+//----------------------------------
+
+// Bật đèn khi trời tối
+int cambien = 10;// khai báo chân digital 10 cho cảm biến
+
+int Led = 8;//kháo báo chân digital 8 cho đèn LED
+//======================
+
 void setup()
 {
+  // Đo nhiệt độ, độ ẩm ===================
   Serial.begin(9600);
 
   dht.begin(); //Khởi động cảm biến
@@ -23,12 +36,24 @@ void setup()
   lcd.print("DO AM:");
   lcd.setCursor(0, 1);
   lcd.print("NHIET DO:");
+  //----------------------------------
+
+  // Vỗ tay bật tắt đèn-------
+  pinMode(soundSensor, INPUT);
+  pinMode(LED, OUTPUT);
+
+  //----------------------------------
+
+  // Tu dong bat den khi troi toi
+  pinMode(Led, OUTPUT); //pinMode xuất tín hiệu đầu ra cho led
+  pinMode(cambien, INPUT); //pinMode nhận tín hiệu đầu vào cho cảm biê
+  //--------------------------------------
 }
 
 void loop()
 
-// Đo nhiệt độ, độ ẩm
 {
+  // Đo nhiệt độ, độ ẩm ===================
   float doam = dht.readHumidity(); //Đọc độ ẩm
 
   float doC = dht.readTemperature(); //Đọc nhiệt độ C
@@ -59,4 +84,29 @@ void loop()
   lcd.print("*C");
 
   delay(1000);
+  //==================================
+
+  // Vỗ tay bật tắt đèn
+  int SensorData = digitalRead(soundSensor);
+  if (SensorData == 1) {
+
+    if (LEDStatus == false) {
+      LEDStatus = true;
+      digitalWrite(LED, HIGH);
+      Serial.print("Đã bật đèn! ");
+    }
+    else {
+      LEDStatus = false;
+      digitalWrite(LED, LOW);
+      Serial.print("Đã tắt đèn! ");
+    }
+  }
+  //===========================
+
+  // Bật đèn khi trời tối
+  int value = digitalRead(cambien);//lưu giá trị cảm biến vào biến value
+
+  digitalWrite(Led, value); //xuất giá trị ra đèn LED
+  //======================================
+
 }
